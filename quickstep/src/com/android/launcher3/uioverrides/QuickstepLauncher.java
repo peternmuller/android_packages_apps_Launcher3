7ea1cad7c266dee549195f61c8c4fe085bce067b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-14 06:57:10 +0530

    Launcher3: Introduce faster status bar expansion
    Directly expand the notifications panel instead of relying on our
    drag velocity. Also expand QS if swiped down on the right side of
    workspace (similar to QS quick pulldown on status bar).
    
    Depend on our existing "Quick QS pulldown" setting.
    
    Change-Id: Idc82f69d9a69415ebe3815d95daea9279873f461

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -232,6 +232,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer 
     // Will be updated when dragging from taskbar.
     private @Nullable UnfoldTransitionProgressProvider mUnfoldTransitionProgressProvider;
     private @Nullable LauncherUnfoldAnimationController mLauncherUnfoldAnimationController;
+    private @Nullable StatusBarTouchController mStatusBarTouchController;
 
     private SplitSelectStateController mSplitSelectStateController;
     private SplitWithKeyboardShortcutController mSplitWithKeyboardShortcutController;
@@ -544,6 +545,10 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer 
             mSplitSelectStateController.onDestroy();
         }
 
+        if (mStatusBarTouchController != null) {
+            mStatusBarTouchController.onDestroy();
+        }
+
         super.onDestroy();
         mHotseatPredictionController.destroy();
         mSplitWithKeyboardShortcutController.onDestroy();
@@ -632,7 +637,8 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer 
         }
 
         if (!getDeviceProfile().isMultiWindowMode) {
-            list.add(new StatusBarTouchController(this));
+            mStatusBarTouchController = new StatusBarTouchController(this);
+            list.add(mStatusBarTouchController);
         }
 
         list.add(new LauncherTaskViewController(this));
